53d50bdff2a677450e1bb84599e4bc01b45fd1f3
aiyk <aiyk.ekwe@gmail.com>
2018-10-18 10:57:49 +0100

    app configuration refactor

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,9 @@
+const config = require('config');
 const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const keys = require('../../config/keys');
 const passport = require('passport');
 
 // Load Input Validation
@@ -92,7 +92,7 @@ router.post('/login', (req, res) => {
         // Sign Token
         jwt.sign(
           payload,
-          keys.secretOrKey,
+          config.get('secretOrKey'),
           { expiresIn: 3600 },
           (err, token) => {
             res.json({
