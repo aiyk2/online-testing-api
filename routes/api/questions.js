f2bcc9c01ff942b552597f4d6305f331e10a03ab
aiyk <aiyk.ekwe@gmail.com>
2018-10-27 23:10:37 +0100

    questions route 97% work in progress

--- routes/api/questions.js
+++ routes/api/questions.js
@@ -9,16 +9,6 @@ const validateQuestionInput = require("../../validation/question");
 // Load Test Model
 const Test = require("../../models/Test");
 
-/*
-
-POST Question
-GET Test Questions
-GET Question
-EDIT Question
-DELETE Question
-
-*/
-
 // @route   GET api/question/test
 // @desc    Tests question route
 // @access  Public
@@ -42,20 +32,37 @@ router.post(
     //find test to add question to
     Test.findOne({ _id: req.body.test_id }).then(test => {
       if (test) {
-        const questionFields = {};
-        questionFields.createdBy = req.user.id;
-        questionFields.test_id = req.body.test_id;
-        questionFields.question = req.body.question;
-        questionFields.options = req.body.options;
-        questionFields.answer = req.body.answer;
-        questionFields.duration = req.body.duration;
 
-        test.questions.push(questionFields);
-        test.save()
-          .then(test => res.json(test))
-          .catch(err =>
-            res.status(500).json("saving question failed")
-          );
+        if(req.body.question_id){
+          const qst = test.questions.id(req.body.question_id);
+
+          qst.createdBy = req.user.id;
+          qst.question = req.body.question;
+          qst.options = req.body.options;
+          qst.answer = req.body.answer;
+          qst.duration = req.body.duration;
+          test.save()
+            .then(test => res.json(test))
+            .catch(err =>
+              res.status(500).json("Question update failed")
+            );
+        } else{
+
+          const questionFields = {};
+          questionFields.createdBy = req.user.id;
+          questionFields.test_id = req.body.test_id;
+          questionFields.question = req.body.question;
+          questionFields.options = req.body.options;
+          questionFields.answer = req.body.answer;
+          questionFields.duration = req.body.duration;
+
+          test.questions.push(questionFields);
+          test.save()
+            .then(test => res.json(test))
+            .catch(err =>
+              res.status(500).json("saving question failed")
+            );
+        }
       } else {
         return res.status(404).json("Test was not found");
       }
@@ -88,79 +95,60 @@ router.post(
   }
 );
 
-// @route   GET api/test/all
-// @desc    Get all tests
-// @access  Public
-router.get("/all", (req, res) => {
-  Test.find()
-    .then(tests => {
-      if (!tests) {
-        return res.status(404).json("There are no tests");
-      }
-
-      res.json(tests);
-    })
-    .catch(err => res.status(404).json("There are no tests"));
-});
-
-// @route   GET api/test/count
-// @desc    Get the total number of tests
-// @access  Public
-router.get("/count", (req, res) => {
-  Test.find()
-    .count()
-    .then(count => {
-      if (!count) {
-        return res.status(404).json("There are no tests");
-      }
-
-      res.json(count);
-    })
-    .catch(err => res.status(404).json("There are no tests"));
-});
-
-// @route   GET api/test/id/:id
-// @desc    Get test by id
-// @access  Public
-
-router.get("/id/:_id", (req, res) => {
-  Test.findOne({ _id: req.params._id })
-    .then(test => {
-      if (!test) {
-        res.status(404).json("The specified test was not found");
-      }
-
-      res.json(test);
-    })
-    .catch(err => res.status(404).json("The specified test was not found"));
-});
-
-// @route   GET api/test/user/:user_id
-// @desc    Get tests by user ID
-// @access  Public
-
-router.get("/user/:user_id", (req, res) => {
-  Test.find({ createdBy: req.params.user_id })
-    .then(test => {
-      if (!test) {
-        res.status(404).json("There is no test for this user");
-      }
-
-      res.json(test);
-    })
-    .catch(err => res.status(404).json("There is no test for this user"));
-});
-
-// @route   DELETE api/Test
-// @desc    Delete Test
+// @route   POST api/test/question/id/:_id
+// @desc    get test question by id
 // @access  Private
-router.delete(
+router.post(
   "/id/:_id",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
-    Test.findOneAndRemove({ _id: req.params._id })
-      .then(() => res.json({ success: true }))
-      .catch(err => res.status(404).json("Could not delete test"));
+
+    Test.findOne({ _id: req.body.test_id }).then(test => {
+      if (test) {
+        const qst = test.questions.id(req.body.question_id);
+        res.json(qst);
+      } else {
+        return res.status(404).json("Test was not found");
+      }
+    });
+  }
+);
+
+// @route   POST api/test/question/count
+// @desc    return the number of questions in test
+// @access  Private
+router.post(
+  "/count",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+
+    //find test and return the total number of questions in the test
+    Test.findOne({ _id: req.body.test_id }).then(test => {
+      if (test) {
+        res.json(test.questions.length)
+      } else {
+        return res.status(404).json("Test was not found");
+      }
+    });
+  }
+);
+
+// @route   POST api/test/question/all
+// @desc    return test questions
+// @access  Private
+router.post(
+  "/all",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+
+    //find test and return its questions
+    Test.findOne({ _id: req.body.test_id }).then(test => {
+      if (test) {
+        res.json(test.questions)
+      } else {
+        return res.status(404).json("Test was not found");
+      }
+    });
   }
 );
 
--- validation/question.js
+++ validation/question.js
@@ -6,7 +6,8 @@ module.exports = function validateQuestionInput(data) {
         options: Joi.array().min(1).max(1000),
         answer: Joi.number().required().min(1).max(5),
         duration: Joi.number(),
-        test_id: Joi.string().min(1).required()
+        test_id: Joi.string().min(1).required(),
+        question_id: Joi.string().min(1)
     };
 
     return result = Joi.validate(data, schema);
